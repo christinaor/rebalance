d2dcf064733acb0668c66a73c339df749f810d94
Christina Or <christinaor.tech@gmail.com>
2022-07-17 23:30:43 -0400

    Add login borders

--- client/components/LoginPage.jsx
+++ client/components/LoginPage.jsx
@@ -16,20 +16,22 @@ const LoginPage = (props) => {
   return (
     <div className="login-page-wrapper">
       <div className="login-container">
-        <div className="login-left">
-          <img className="center" src={piggybank} alt="minimalist piggy bank image" />
-        </div>
+          <img className="login-left" src={piggybank} alt="minimalist piggy bank image" />
 
         <div className="login-right">
-          <div className="login-title">rebalance</div>
-          <form className="login-form" id="signin" action="/signin" method="POST">
-            <input id="user" name="user" placeholder="Email" type="text"></input>
-            <input id="pass" name="pass" placeholder="Password" type="text"></input>
-            <button id="login-submit" type="submit">Log In!</button>
-          </form>
+          <div className="login-only-wrapper">
+            <div className="login-title">rebalance</div>
+            <form className="login-form" id="signin" action="/signin" method="POST">
+              <input id="user" name="user" placeholder="Email" type="text"></input>
+              <input id="pass" name="pass" placeholder="Password" type="text"></input>
+              <button id="login-submit" type="submit">Log In!</button>
+            </form>
+          </div>
           <br />
-          <div className="login-signup-text">New to rebalance?</div>
-          <button id="signup-submit" type="submit">Sign Up Here!</button>
+          <div className="login-signup-wrapper">
+            <div className="login-signup-text">New to rebalance?</div>
+            <button id="signup-submit" type="submit">Sign Up Here!</button>
+          </div>
         </div>
       </div>
     </div>
