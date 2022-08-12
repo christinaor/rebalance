7d5e0cfbdd722a915581671563e4ddb123c87166
Christina Or <christinaor.tech@gmail.com>
2022-08-12 12:04:36 -0400

    Improve readability by separating map from return

--- client/components/ListOfRecords.jsx
+++ client/components/ListOfRecords.jsx
@@ -13,6 +13,7 @@ import React, { useEffect, useState } from "react";
 import Record from "./Record.jsx";
 
 const ListOfRecords = props => {
+  const [recordsData, setRecordsData] = useState([]);
   const [recordsList, setRecordsList] = useState([]);
   const [populatedRecords, setPopulatedRecords] = useState(false);
 
@@ -29,68 +30,22 @@ const ListOfRecords = props => {
       .catch((err) => `Error getting records: ${err}`)
   }, []);
 
-  // const recordElements = [];
-  // for (let record )
-
-  // const recordElements = recordsList.map(record => {(
-  //   <Record 
-  //     key={`record${record.id}`}
-  //     input_date={record.input_date}
-  //     username={record.username}
-  //     item_name={record.item_}
-  //   />
-  // )})
-  // const recordElements = [];
-  // if (populatedRecords) {
-  //   recordElements.push(record => {return (<Record 
-  //     id={`record${record.id}`}
-  //     input_date={record.input_date}
-  //     username={record.username}
-  //     item_name={record.item_name}
-      
-  //     />)
-  //   })
-  // }
-  // const listArray = list.map(num => <div>{num}</div>)
-  // const getList = () => {
-  //   if (!list.length) {
-  //     fetch('/api')
-  //       .then(response => response.json())
-  //       .then(data => {
-
-  //       })
-  //   };
-  //   console.log('returned list from getList func')
-  // }
-
-  // const getList = () => {
-  //   fetch('/api')
-  //     // don't need to read text as json - will crash in the front
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       const someData = data;
-  //       console.log(someData);
-  //       return someData;
-  //     })
-  // }
-  // getList();
+  const recordElements = recordsList.map(record => {
+    const { id, input_date, username, item_name } = record;
+    return (
+      <Record 
+        id={`record${id}`}
+        input_date={input_date}
+        username={username}
+        item_name={item_name}
+      />
+    )
+  });
 
   return (
     <div>
       List of Records:
-      {/* {populatedRecords && recordElements} */}
-      {recordsList.map(record => {
-        return (
-          <Record 
-            key={`record${record.id}`}
-            id={`record${record.id}`}
-            input_date={record.input_date}
-            username={record.username}
-            item_name={record.item_name}
-          />
-        )
-      })}
-      {/* {populatedRecords && <div>{recordsList[0].id}</div>} */}
+      {recordElements}
     </div>
   )
 };
