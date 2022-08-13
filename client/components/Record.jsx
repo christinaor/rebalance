0a22d4f32306c7a391df83e08a4707818e325c15
Christina Or <christinaor.tech@gmail.com>
2022-08-12 22:25:13 -0400

    Pick different test columns for records

--- client/components/Record.jsx
+++ client/components/Record.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 const Record = (props) => {
-  const { id, input_date, username, item_name } = props;
+  const { id, input_date, username, item_name, cost } = props;
 
   return (
-    <div>This is {id}, where {username} purchased {item_name}!</div>
+    <div id="record">
+      <div>{id}</div>
+      <div>{input_date}</div>
+      <div>{item_name}</div>
+      <div>{cost}</div>
+    </div>
   )
 };
 
