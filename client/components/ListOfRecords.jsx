b7c34c3f9f5ec421a8c569e241c52c7016cf9244
Christina Or <christinaor.tech@gmail.com>
2022-08-17 19:21:53 -0400

    Refactor update and add props

--- client/components/ListOfRecords.jsx
+++ client/components/ListOfRecords.jsx
@@ -12,43 +12,19 @@
 import React, { useEffect, useState } from "react";
 import Record from "./Record.jsx";
 
+
 const ListOfRecords = props => {
-  const [recordsList, setRecordsList] = useState([]);
-  const [populatedRecords, setPopulatedRecords] = useState(false);
-  const [togglePostRecordForm, setTogglePostRecordForm] = useState(false);
-  const [postedRecord, setPostedRecord] = useState({
-    name: '',
-    item: '',
-    cost: ''
-  });
+  const {
+    recordsList,
+    setRecordsList,
+    populatedRecords,
+    setPopulatedRecords,
+    updatedRecord,
+    setUpdatedRecord,
+    toUpdate,
+    setToUpdate
+  } = props;
   const [actionsValue, setActionsValue] = useState('Select')
-  const [toUpdate, setToUpdate] = useState(false);
-  const [updatedRecord, setUpdatedRecord] = useState({
-    id: 1,
-    item: null,
-    cost: null
-  });
-
-  // RecordsList is rendered upon initial render
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    fetch('/api/records', {
-      signal: signal
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('get data here: ', data);
-        setRecordsList(data);
-      })
-      .catch((err) => {
-        if (err.name === 'AbortError') {
-          return 'Successfully aborted!';
-        } else return `Error getting records: ${err}`
-      })
-      .finally(setPopulatedRecords(true))
-  }, [populatedRecords]);
 
   const recordElements = recordsList.map(record => {
     const { id, input_date, username, item_name, item_cost } = record;
@@ -72,58 +48,9 @@ const ListOfRecords = props => {
     )
   });
 
-  const postRecord = (e) => {
-    console.log('postRecord fired')
-    e.preventDefault();
-    fetch('/api/records', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: postedRecord.name,
-        item_name: postedRecord.item,
-        item_cost: postedRecord.cost
-      })
-    })
-      .then(() => {
-        console.log('added record!')
-        setPopulatedRecords(false);
-      })
-      .catch(err => `Error adding record: ${err}`)
-      .finally(
-        setPostedRecord({
-          name: '',
-          item: '',
-          cost: ''
-        })
-      )
-  };
-
-  const updateRecord = (e) => {
-    console.log('updateRecord fired');
-    e.preventDefault();
-    fetch('api/records/', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        id: updatedRecord.id,
-        item_name: updatedRecord.item,
-        item_cost: updatedRecord.cost
-      })
-    })
-      .then(() => {
-        setPopulatedRecords(false);
-      })
-      .catch(err => `Error updating record: ${err}`)
-      .finally(setToUpdate(false))
-  };
-
   return (
-    <div>
-      List of Records:
+    <div className="records-container">
+      <h2>List of Records:</h2>
       <div className="records-grid-container">
         <div className="record-list-titles grid-record">
           <span>Record No.</span>
@@ -135,63 +62,6 @@ const ListOfRecords = props => {
         {recordElements}
       </div>
       <br />
-
-      <div className="all-forms">
-        <div className="post-form">
-          <button onClick={() => setTogglePostRecordForm(!togglePostRecordForm)}>{togglePostRecordForm ? `Exit Adding A Record`: `Add A Record Here`}</button>
-          {
-          togglePostRecordForm && <form action="/api/records" method="POST">
-            <div>
-              <label for="name">Name:</label>
-              <input name="name" type="text" value={postedRecord.name} id="records-post-name" onChange={(e) => setPostedRecord({...postedRecord, name: e.target.value})} />
-            </div>
-            <div>
-              <label for="item">Item Purchased:</label>
-              <input name="item" type="text" value={postedRecord.item} id="records-post-item" onChange={(e) => setPostedRecord({...postedRecord, item: e.target.value})} />
-            </div>
-            <div>
-              <label for="cost">Item Cost:</label>
-              <input name="cost" type="text" value={postedRecord.cost} id="records-post-cost" onChange={(e) => setPostedRecord({...postedRecord, cost: e.target.value})} />
-            </div>
-            <div>
-              <button type="submit" onClick={postRecord}>Add A Record</button>
-            </div>
-          </form>
-          }
-        </div>
-
-        {!toUpdate && 
-          <div className="update-form-placeholder">Select an action to update/delete a record.</div>
-        }
-        {toUpdate &&
-          <div className="updateForm">
-            {`Update Record No. ${updatedRecord.id}:`}
-            <form action="/api/records" method="PUT">
-            <div>
-              <label for="item">Change Item:</label>
-              <input name="item" type="text" id="records-update-item" onChange={(e) => {
-                if (e.target.value !== '') {
-                  setUpdatedRecord({...updatedRecord, item: e.target.value})
-                }
-              }} />
-            </div>
-            <div>
-              <label for="cost">Change Cost:</label>
-              <input name="cost" type="text" id="records-update-cost" onChange={(e) => {
-                if (e.target.value !== '') {
-                  setUpdatedRecord({...updatedRecord, cost: e.target.value})
-                }
-              }} />
-            </div>
-            <div>
-              <button type="submit" onClick={updateRecord}>Update Record No. {updatedRecord.id}</button>
-              <button onClick={() => setToUpdate(false)}>Cancel Update</button>
-              {console.log('in ListOfRecords render checking updatedRecord: ', updatedRecord)}
-            </div>
-          </form>
-          </div>
-        }
-      </div>
     </div>
   )
 };
--- client/containers/RecordsContainer.jsx
+++ client/containers/RecordsContainer.jsx
@@ -11,11 +11,40 @@
 
 import React, { useEffect, useState } from "react";
 import AddRecord from "../components/AddRecord.jsx";
-// import ListOfRecords from '../components/ListOfRecords.jsx';
+import UpdateRecord from "../components/UpdateRecord.jsx";
+import ListOfRecords from '../components/ListOfRecords.jsx';
 
 const RecordsContainer = props => {
   const [recordsList, setRecordsList] = useState([]);
   const [populatedRecords, setPopulatedRecords] = useState(false);
+  const [toUpdate, setToUpdate] = useState(false);
+  const [updatedRecord, setUpdatedRecord] = useState({
+    id: 1,
+    item: null,
+    cost: null
+  });
+
+
+  // RecordsList is rendered upon initial render
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch('/api/records', {
+      signal: signal
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('get data here: ', data);
+        setRecordsList(data);
+      })
+      .catch((err) => {
+        if (err.name === 'AbortError') {
+          return 'Successfully aborted!';
+        } else return `Error getting records: ${err}`
+      })
+      .finally(setPopulatedRecords(true))
+  }, [populatedRecords]);
 
   return (
     <div>
@@ -25,7 +54,26 @@ const RecordsContainer = props => {
         populatedRecords={populatedRecords}
         setPopulatedRecords={setPopulatedRecords}
       />
-      {/* <ListOfRecords /> */}
+      <ListOfRecords 
+        recordsList={recordsList}
+        setRecordsList={setRecordsList}
+        populatedRecords={populatedRecords}
+        setPopulatedRecords={setPopulatedRecords}
+        updatedRecord={updatedRecord}
+        setUpdatedRecord={setUpdatedRecord}
+        toUpdate={toUpdate}
+        setToUpdate={setToUpdate}
+      />
+      <UpdateRecord 
+        recordsList={recordsList}
+        setRecordsList={setRecordsList}
+        populatedRecords={populatedRecords}
+        setPopulatedRecords={setPopulatedRecords}
+        updatedRecord={updatedRecord}
+        setUpdatedRecord={setUpdatedRecord}
+        toUpdate={toUpdate}
+        setToUpdate={setToUpdate}
+      />
     </div>
   )
 }
