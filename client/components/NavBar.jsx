22673805bd4b9fb8e1fe4c101827ad91448555ad
Christina Or <christinaor.tech@gmail.com>
2022-08-22 22:42:26 -0400

    Set up initial NavBar

---
+++ client/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from "react";
+import { useNavigate, Routes, Route, Navigate } from 'react-router-dom';
+
+const NavBar = (props) => {
+  return (
+    <div>Initial NavBar Component</div>
+  )
+};
+
+export default NavBar;
