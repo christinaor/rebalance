ba1f91e0f08ada52b319ef2c9ae756ea07eec578
Christina Or <christinaor.tech@gmail.com>
2022-08-23 23:50:36 -0400

    Add initial navbar routes

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,9 +1,31 @@
+/**
+ * ************************************
+ *
+ * @module  NavBar
+ * @author
+ * @date
+ * @description NavBar is a component with different routes leading the user to different pages.
+ *
+ * ************************************
+ */
+
 import React, {useEffect, useState} from "react";
-import { useNavigate, Routes, Route, Navigate } from 'react-router-dom';
+import { useNavigate, Routes, Route, Navigate, Link } from 'react-router-dom';
 
 const NavBar = (props) => {
   return (
-    <div>Initial NavBar Component</div>
+    <div>
+      <li>
+        Welcome to Rebalance!
+      </li>
+      <li>
+        <Link to="/settings">Settings</Link>
+      </li>
+      <li>
+        <Link to="/login">Logout</Link>
+      </li>
+    </div>
+
   )
 };
 
