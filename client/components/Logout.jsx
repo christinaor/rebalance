252ebf17e8c85aef71a3a47cb398f26360a1e86a
Christina Or <christinaor.tech@gmail.com>
2022-08-24 17:43:53 -0400

    Implement logout functionality in logout button

---
+++ client/components/Logout.jsx
@@ -0,0 +1,36 @@
+/**
+ * ************************************
+ *
+ * @module  Logout
+ * @author
+ * @date
+ * @description Logout is the functionality that logs out a user and erases the cookie.
+ *
+ * ************************************
+ */
+
+ import React, {useEffect, useState} from "react";
+ 
+ const Logout = (props) => {
+  const { 
+    cookies, 
+    removeCookie, 
+    setCookie, 
+    isLoggedIn, 
+    setIsLoggedIn,
+    cookieTimeout,
+    setCookieTimeout
+  } = props;
+
+  const logoutNow = (e) => {
+    setCookieTimeout(removeCookie('user', { path: '/' }));
+    setIsLoggedIn(false);
+  }
+  return (
+    <button className="nav-logout-button" onClick={logoutNow}>
+      Logout
+    </button>
+  )
+};
+ 
+ export default Logout;
