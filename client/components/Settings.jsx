a491169d0065edc4e3dac46a4f8f96f3fd64221e
Christina Or <christinaor.tech@gmail.com>
2022-08-23 23:57:49 -0400

    Create initial Settings page where user changes credentials

---
+++ client/components/Settings.jsx
@@ -0,0 +1,20 @@
+/**
+ * ************************************
+ *
+ * @module  Settings
+ * @author
+ * @date
+ * @description Settings page for user to change credentials
+ *
+ * ************************************
+ */
+
+ import React, {useEffect, useState} from "react";
+ 
+ const Settings = (props) => {
+   return (
+    <div>Initial Settings Page</div>
+   )
+ };
+ 
+ export default Settings;
