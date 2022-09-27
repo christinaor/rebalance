e238b93252e24691374441c9c5ce4eb2bb16b0c8
Christina Or <christinaor.tech@gmail.com>
2022-09-27 10:55:54 -0400

    Add source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: process.env.NODE_ENV,
@@ -39,11 +40,12 @@ module.exports = {
       },
     ]
   },
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
       template: 'index.html'
-    })
+    }),
   ],
   devServer: {
     static: {
