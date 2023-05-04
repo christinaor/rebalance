958bb23567689a0db981b4e97767b63ca31a6354
Christina Or <christinaor.tech@gmail.com>
2023-05-04 00:32:52 -0400

    Update Summary section

--- client/containers/TotalBalanceContainer/TotalBalanceContainer.jsx
+++ client/containers/TotalBalanceContainer/TotalBalanceContainer.jsx
@@ -10,8 +10,10 @@
  */
 
 import React, { useEffect, useState } from "react";
-import { Paper } from '@mui/material';
+
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+
+import DownArrow from '../../../assets/down-arrow.svg';
 
 import styles from './styles.module.scss';
 
@@ -65,61 +67,40 @@ export const TotalBalanceContainer = (props) => {
         <div className={styles.balances}>
           <div className={styles.userBalanceAndDueDate}>
             <div className={styles.userBalance}>
-              <h3 className={styles.userBalanceTitle}>You paid:</h3>
+              <h3 className={styles.userBalanceTitle}>Your portion:</h3>
               <h3 className={styles.userBalanceAmount}>${userBalance}</h3>
             </div>
+            
             <h3 className={styles.dueDateText}>
-              {userBalance > 0 
-                ? <span>Due by <span className={styles.date}>{lastDayWithoutTime}</span></span>
-                : <span>Nothing to reconcile!</span>
-              }
+              {userBalance > counterpartyBalance 
+              ? <div>
+                <div>You owe {currentCounterparty} ${reconciliationAmount}.</div>
+                <div>Due by <span className={styles.date}>{lastDayWithoutTime}</span></div>
+              </div>
+              : <span>Less than {currentCounterparty}. You're clear!</span>}
             </h3>
           </div>
 
+          {userBalance > counterpartyBalance
+          ? <DownArrow className={styles.downArrowToCounterParty} />
+          : <DownArrow className={styles.downArrowToUser} />}
+          
           <div className={styles.userBalanceAndDueDate}>
             <div className={styles.userBalance}>
-              <h3 className={styles.userBalanceTitle}>{currentCounterparty} to pay:</h3>
+              <h3 className={styles.userBalanceTitle}>{currentCounterparty}'s portion:</h3>
               <h3 className={styles.userBalanceAmount}>${counterpartyBalance}</h3>
             </div>
             <h3 className={styles.dueDateText}>
-              {counterpartyBalance > 0 
-                ? <span>Due by <span className={styles.date}>{lastDayWithoutTime}</span></span>
-                : <span>Nothing to reconcile!</span>
+              {counterpartyBalance > userBalance 
+              ? <div>
+                <div>{currentCounterparty} must pay ${Math.abs(reconciliationAmount)}.</div>
+                <div>Due by <span className={styles.date}>{lastDayWithoutTime}</span></div>
+              </div>
+              : <span>Nothing to reconcile!</span>
               }
-              
             </h3>
           </div>
         </div>
-            {/* <div className={styles.card}>
-              <h3 className={styles.cardTitle}>Current Balances</h3>
-              <div className={styles.cardDetails}>
-                <div>Your balance: ${userBalance}</div>
-                <div>Balance from {currentCounterparty} {currentCounterparty !== 'All Parties' ? 's' : null}: ${counterpartyBalance}</div>
-                <div>Unpaid Balances: {numUnpaidBalances}</div>
-              </div>
-            </div> */}
-
-            {/* <div className={styles.card}>
-              <h3 className={styles.cardTitle}>Upcoming Due Date</h3>
-              <div className={styles.cardDetails}>
-                <div>{lastDayWithoutTime}</div>
-              </div>
-            </div> */}
-
-        <div className={styles.reconciliationsSummary}>
-          <div className={styles.numberOfBalances}>
-            <h3 className={styles.numberOfBalancesTitle}>Transactions to Reconcile:</h3>
-            <h3 className={styles.numberOfBalancesAmount}>{numUnpaidBalances}</h3>
-          </div>
-          
-          <div className={styles.separator} />
-
-          <div className={styles.totalOfBalances}>
-          <h3 className={styles.totalOfBalancesTitle}>Total to Reconcile:</h3>
-            <h3 className={styles.totalOfBalancesAmount}>${reconciliationAmount}</h3>
-          </div>
-        </div>
-
       </div>
     </section>
   )
