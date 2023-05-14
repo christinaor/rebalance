0ad7ffb905cc1317bd0cbbdd21d0413cf59a65ee
Christina Or <christinaor.tech@gmail.com>
2023-05-14 18:57:33 -0400

    Initial LoginPage style update

--- client/components/LoginPage/LoginPage.jsx
+++ client/components/LoginPage/LoginPage.jsx
@@ -71,88 +71,83 @@ const LoginPage = (props) => {
   };
 
   return (
-    <div className={styles.loginPageWrapper}>
-      {/* <div className="login-top">
-        <span className="nav-logo">RE:Balance</span>
-      </div> */}
-
-      <div className={styles.loginLeft}>
-        <div className={styles.loginTextWrapper}>
-          <Typography variant="h1" className={styles.loginTitleText}>RE:Balance</Typography>
-          <Typography variant="h2" className="login-subtitle-text">Manage reconciliations with your circles.</Typography>
+    <div className={styles.loginPage}>
+      <div className={styles.loginPageMainContent}>
+        <div className={styles.loginLogoAndSubtext}>
+          <h1 className={styles.loginTitleText}><span className={styles.title1}>RE:</span><span>Balance</span></h1>
+          <h2 className={styles.loginSubtext}>Manage reconciliations with your circles.</h2>
         </div>
-        {/* <img className="login-left-img" src={piggybank} alt="minimalist piggy bank image" /> */}
+
+        <div className={styles.loginRight}>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            className="login-only-wrapper"
+          >
+            <TextField
+              id="login-email"
+              label="Email"
+              variant="outlined"
+              value={loginCreds.email}
+              onInput={(e) => setLoginCreds({...loginCreds, email: e.target.value})}
+            />
+            <TextField
+              id="login-pass"
+              label="Password"
+              variant="outlined"
+              onInput={(e) => setLoginCreds({...loginCreds, pass: e.target.value})}
+            />
+            <Button className={styles.loginButton} type="submit" variant="contained" size="medium" onClick={handleLogin}>Log in</Button>
+          </Box>
+
+          {/* <form className="login-form" action="/authorize/signin" method="GET">
+            <input 
+              id="email" 
+              name="email" 
+              placeholder="Email" 
+              type="text" 
+              value={loginCreds.email} 
+              onChange={(e) => setLoginCreds({...loginCreds, email: e.target.value})} 
+            />
+            <input 
+              id="pass" 
+              name="pass" 
+              placeholder="Password" 
+              type={ showPass ? "text" : "password" }
+              value={loginCreds.pass} 
+              onChange={(e) => setLoginCreds({...loginCreds, pass: e.target.value})} 
+            />
+            <button 
+              className="login-submit" 
+              type="submit" 
+              onClick={handleLogin}
+            >Log In!</button>
+          </form> */}
+          <div>
+          { failedLogin &&
+            <p>Forgot password?</p>
+          }
+          </div>
+
+          <hr></hr>
+
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            className={styles.loginSignupWrapper}
+          >
+            <Button className={styles.signupLinkButton} type="submit" variant="contained" size="medium" onClick={() => setPressedSignup(true)}>Create new account</Button>
+          </Box>
+        </div>
       </div>
-
-      <Paper elevation={3} className={styles.loginRight}>
-        <Box
-          component="form"
-          sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-          className="login-only-wrapper"
-        >
-          <TextField
-            id="login-email"
-            label="Email"
-            variant="outlined"
-            value={loginCreds.email}
-            onInput={(e) => setLoginCreds({...loginCreds, email: e.target.value})}
-          />
-          <TextField
-            id="login-pass"
-            label="Password"
-            variant="outlined"
-            onInput={(e) => setLoginCreds({...loginCreds, pass: e.target.value})}
-          />
-          <Button className={styles.loginButton} type="submit" variant="contained" size="medium" onClick={handleLogin}>Log in</Button>
-        </Box>
-
-        {/* <form className="login-form" action="/authorize/signin" method="GET">
-          <input 
-            id="email" 
-            name="email" 
-            placeholder="Email" 
-            type="text" 
-            value={loginCreds.email} 
-            onChange={(e) => setLoginCreds({...loginCreds, email: e.target.value})} 
-          />
-          <input 
-            id="pass" 
-            name="pass" 
-            placeholder="Password" 
-            type={ showPass ? "text" : "password" }
-            value={loginCreds.pass} 
-            onChange={(e) => setLoginCreds({...loginCreds, pass: e.target.value})} 
-          />
-          <button 
-            className="login-submit" 
-            type="submit" 
-            onClick={handleLogin}
-          >Log In!</button>
-        </form> */}
-        <div>
-        { failedLogin &&
-          <p>Forgot password?</p>
-        }
-        </div>
-
-        <hr></hr>
-
-        <Box
-          component="form"
-          sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-          className={styles.loginSignupWrapper}
-        >
-          <Button className={styles.signupLinkButton} type="submit" variant="contained" size="medium" onClick={() => setPressedSignup(true)}>Create new account</Button>
-        </Box>
-      </Paper>
     </div>
   )
 }
