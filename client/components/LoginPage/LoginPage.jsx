5807bebd6769a6f8fc6a32b98bfe98a1f6b27b21
Christina Or <christinaor.tech@gmail.com>
2023-05-15 17:18:55 -0400

    Fix login page style and elements

--- client/components/LoginPage/LoginPage.jsx
+++ client/components/LoginPage/LoginPage.jsx
@@ -9,17 +9,17 @@
  * ************************************
  */
 
-import React, { useEffect, useState } from "react";
-import { Paper, Button, Box, TextField, Typography } from '@mui/material';
+import React, { useState } from "react";
 
 import styles from './styles.module.scss';
 
 const LoginPage = (props) => {
-  const [loginCreds, setLoginCreds] = useState({
+  const [loginCredentials, setLoginCredentials] = useState({
     email: '',
     pass: ''
   })
-  const [showPass, setShowPass] = useState(false);
+  // TODO - show password
+  // const [showPass, setShowPass] = useState(false);
   const [failedLogin, setFailedLogin] = useState(false);
 
   const {
@@ -43,8 +43,8 @@ const LoginPage = (props) => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        email: loginCreds.email,
-        pass: loginCreds.pass
+        email: loginCredentials.email,
+        pass: loginCredentials.pass
       })
     })
       .then(response => response.json())
@@ -63,7 +63,7 @@ const LoginPage = (props) => {
       })
       .catch(err => `Error checking credentials of user login: ${err}`)
       .finally(
-        setLoginCreds({
+        setLoginCredentials({
           email: '',
           pass: ''
         })
@@ -75,77 +75,30 @@ const LoginPage = (props) => {
       <div className={styles.loginPageMainContent}>
         <div className={styles.loginLogoAndSubtext}>
           <h1 className={styles.loginTitleText}><span className={styles.title1}>RE:</span><span>Balance</span></h1>
-          <h2 className={styles.loginSubtext}>Manage reconciliations with your circles.</h2>
+          <h2 className={styles.loginSubtext}>Manage reconciliations with your circles easily.</h2>
         </div>
 
-        <div className={styles.loginRight}>
-          <Box
-            component="form"
-            sx={{
-              '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-            className="login-only-wrapper"
-          >
-            <TextField
-              id="login-email"
-              label="Email"
-              variant="outlined"
-              value={loginCreds.email}
-              onInput={(e) => setLoginCreds({...loginCreds, email: e.target.value})}
-            />
-            <TextField
-              id="login-pass"
-              label="Password"
-              variant="outlined"
-              onInput={(e) => setLoginCreds({...loginCreds, pass: e.target.value})}
-            />
-            <Button className={styles.loginButton} type="submit" variant="contained" size="medium" onClick={handleLogin}>Log in</Button>
-          </Box>
+        <form className={styles.loginForm} action="/authorize/signin" method="POST">
+          <div className={styles.formField}>
+            <label className={styles.formFieldLabel}  htmlFor="login-email">Email:</label>
+            <input className={styles.formFieldInput} name="login-email" type="email" value={loginCredentials.email} onChange={(e) => setLoginCredentials({...loginCredentials, email: e.target.value})} required />
+          </div>
+          <div className={styles.formField}>
+            <label className={styles.formFieldLabel}  htmlFor="login-pass">Password:</label>
+            <input className={styles.formFieldInput} name="login-pass" type="password" value={loginCredentials.pass} onChange={(e) => setLoginCredentials({...loginCredentials, pass: e.target.value})} required />
+          </div>
+          <button className={styles.loginButton} onClick={handleLogin}>LOG IN</button>
+        </form>
 
-          {/* <form className="login-form" action="/authorize/signin" method="GET">
-            <input 
-              id="email" 
-              name="email" 
-              placeholder="Email" 
-              type="text" 
-              value={loginCreds.email} 
-              onChange={(e) => setLoginCreds({...loginCreds, email: e.target.value})} 
-            />
-            <input 
-              id="pass" 
-              name="pass" 
-              placeholder="Password" 
-              type={ showPass ? "text" : "password" }
-              value={loginCreds.pass} 
-              onChange={(e) => setLoginCreds({...loginCreds, pass: e.target.value})} 
-            />
-            <button 
-              className="login-submit" 
-              type="submit" 
-              onClick={handleLogin}
-            >Log In!</button>
-          </form> */}
-          <div>
+        <>
           { failedLogin &&
             <p>Forgot password?</p>
           }
-          </div>
+        </>
 
-          <hr></hr>
-
-          <Box
-            component="form"
-            sx={{
-              '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-            className={styles.loginSignupWrapper}
-          >
-            <Button className={styles.signupLinkButton} type="submit" variant="contained" size="medium" onClick={() => setPressedSignup(true)}>Create new account</Button>
-          </Box>
+        <div className={styles.newMemberSignup}>
+          <span>New member?</span>
+          <button className={styles.loginPageSignupButton} onClick={() => setPressedSignup(true)}>SIGN UP</button>
         </div>
       </div>
     </div>
